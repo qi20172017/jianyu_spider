1a48615bf25bc48b1b76066c300401fe2e4919dc
qifumin <fumin.qi@aisuperstar.com>
2021-05-31 15:40:32 +0800

    添加注释

--- model/db_connects.py
+++ model/db_connects.py
@@ -1,7 +1,9 @@
+# 这里是数据库的链接地址，根据自己的情况添加
+
 RDS_HOST = '********'
 RDS_PORT = 8888
 RDS_DB = 8888
 RDS_PASSWORD = '**********'
 
-PG_ENGINE = 'postgresql+psycopg2://****************'
-MYSQL_ENGINE = 'mysql+mysqlconnector://****************'
+PG_ENGINE = '****************'
+MYSQL_ENGINE = '****************'
