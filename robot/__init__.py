e2627c526910fd5d319d139ae6de6db2f4d044d3
qifumin <fumin.qi@weimob.com>
2023-08-16 20:19:30 +0800

    整理剑鱼

---
+++ robot/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File  : __init__.py.py
+@Author: crawlSpider
+@Address: https://weixin.sogou.com/weixin?type=1&s_from=input&query=%E7%BD%91%E8%99%ABspider&ie=utf8&_sug_=n&_sug_type_=
+@Github: https://github.com/qi20172017
+@Date  : 2023/8/16 下午7:57
+@Desc  : 
+"""
+
+if __name__ == '__main__':
+    pass
